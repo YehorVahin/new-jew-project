001f5f0c2dcda38bdb9650c040703890db499f04
Yehor Vahin <davyjohns322@gmail.com>
2025-04-27 21:50:54 +0300

    finished all

--- src/components/ShowFullItem.jsx
+++ src/components/ShowFullItem.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 
-export class Item extends Component {
+export class ShowFullItem extends Component {
   render() {
     return (
-      <div className='item'>
+      <div className='full-item'>
+        <div className=''>
         <img src={"./img/" + this.props.item.img} alt="" onClick={() => this.props.onShowItem(this.props.item)} />
         <h2>{this.props.item.title}</h2>
         <p>{this.props.item.desc}</p>
         <b>{this.props.item.price}</b>
         <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+        </div>
       </div>
     )
   }
 }
 
-export default Item
+export default ShowFullItem
